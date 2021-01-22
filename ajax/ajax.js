cf4ee3bd418a191c8cffa10f44e1b923c1a8ec7f
AzizaAlhouni <azizaalhouni@yahoo.com>
2021-01-22 15:25:28 -0600

    added ajax

---
+++ ajax/ajax.js
@@ -0,0 +1,13 @@
+(function (){
+    "use strict";
+    window.addEventListener("load",function(){
+        document.querySelector('#hw').addEventListener("change", function(){
+            let file = 'aziza/' + document.querySelector('#hw').value;
+            fetch(file)
+            .then(res=>res.text())
+            .then(data=>{
+                document.querySelector('#output').value= data;
+        })
+    });
+});
+})();
