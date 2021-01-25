8e184466f7401711f7696d14b36b527d87638992
AzizaAlhouni <azizaalhouni@yahoo.com>
2021-01-25 17:17:30 -0600

    added jason.html and js

---
+++ ajax/jason.js
@@ -0,0 +1,17 @@
+async function createUser(){
+    let user = {
+        name = 'John',
+        job = 'coder'
+    };
+    let response = await fetch('http://reqres.in/api/users/',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+    });
+    let result = await response.json();
+    alert(`User ${result.name} is created at ${result.createdAt}`);
+
+}
+createUser();
